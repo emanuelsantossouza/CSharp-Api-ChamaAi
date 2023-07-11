f2af1d06183149f21191729359fe465f36ef2288
emanuel santoss <emanuel.jesus3@etec.sp.gov.br>
2023-07-11 17:28:08 -0300

    projeto-pronto

--- api/Program.cs
+++ api/Program.cs
@@ -19,11 +19,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
 
 app.UseHttpsRedirection();
